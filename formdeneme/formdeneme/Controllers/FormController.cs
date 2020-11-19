e80ac9d8910c1e7f8a39e381fa73a04056d2b815
smskmrym <71767389+smskmrym@users.noreply.github.com>
2020-11-19 19:22:55 +0300

    1

---
+++ formdeneme/formdeneme/Controllers/FormController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using formdeneme.Models.DomainModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace formdeneme.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            var book = new Form();
+            book.Title = "";
+            book.Description = "";
+            book.SamplePage = "";
+            book.AuthorEmail = "";
+            book.AuthorPhone = "";
+
+            return View(book);
+        }
+
+        public IActionResult Create()
+        {
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Form book)
+        {
+            if (ModelState.IsValid == true)
+            {
+                // business logic ...
+            }
+            else
+            {
+                // let user re-input the data
+            }
+
+            return new JsonResult(book);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Form book)
+        {
+            if (ModelState.IsValid == true)
+            {
+                // business logic ...
+            }
+            else
+            {
+                // let user re-input the data
+            }
+
+            return new JsonResult(book);
+        }
+    }
+}
---
+++ formdeneme/formdeneme/Models/DomainModel/Form.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace formdeneme.Models.DomainModels
+{
+    public class Form
+    {
+
+
+        [Required, StringLength(100)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Required, Range(0, 999.99)]
+
+        public string SamplePage { get; set; }
+
+        [EmailAddress]
+        public string AuthorEmail { get; set; }
+
+        [Phone]
+        public string AuthorPhone { get; set; }
+    }
+}
